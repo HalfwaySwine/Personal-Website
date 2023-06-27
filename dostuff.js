336f14dc7307b62934d7790d852ce2999771ff31
Lee Forberg <123601328+HalfwaySwine@users.noreply.github.com>
2023-06-27 17:02:30 -0400

    Update dostuff.js

--- dostuff.js
+++ dostuff.js
@@ -1,12 +1,11 @@
 function scrollfunction(){ 
-    console.log('dsdssddsdsdss') 
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    
+
     const nav = document.getElementById("navbar");
-    nav.style.background = "black";  
+    nav.style.background = "#363538";  
     nav.style.transition = "all 0.5s ease-out"
   } else {
-   
+
     document.getElementById("navbar").style.background = "none";
   }
 }
